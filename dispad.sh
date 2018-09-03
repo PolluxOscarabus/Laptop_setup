da3f4d7cef1432461c02cb8b62cae50455be5245
PolluxOscarabus <polluxoscarabus@gmail.com>
2018-09-03 13:34:33 -0500

    Create touchpad disabling script

---
+++ dispad.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##################################################
+#
+# Script disabling laptop touchpad
+#
+##################################################
+
+echo -e "Enter 0 to disable touchpad or 1 to enable it."
+read -p "option : " CHOICE 
+if [ "$CHOICE" == 0 ] || [ "$CHOICE" == 1 ]; then
+  ID="$(xinput list --id-only "SynPS/2 Synaptics TouchPad")"
+  xinput set-prop $ID "Device Enabled" $CHOICE
+else
+  echo -e "Invalid option.\nExit script."
+fi
+
